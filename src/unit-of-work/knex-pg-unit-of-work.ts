0181950692331a377d9eabb7325ec0b254eb5c19
nivinjoseph <nivinjoseph@outlook.com>
2019-07-17 21:31:43 -0400

    refactorings

--- src/unit-of-work/knex-pg-unit-of-work.ts
+++ src/unit-of-work/knex-pg-unit-of-work.ts
@@ -12,66 +12,69 @@ export class KnexPgUnitOfWork implements UnitOfWork
 {
     private readonly _dbConnectionFactory: DbConnectionFactory;
     private _transactionScope: TransactionScope;
-    
-    
+
+
     public constructor(dbConnectionFactory: DbConnectionFactory)
     {
         given(dbConnectionFactory, "dbConnectionFactory").ensureHasValue();
-        
+
         this._dbConnectionFactory = dbConnectionFactory;
     }
-    
+
     public getTransactionScope(): Promise<object>
     {
         if (this._transactionScope)
         {
             if (this._transactionScope.isCommitted || this._transactionScope.isRolledBack)
-                return Promise.reject(new InvalidOperationException("using completed UnitOfWork"));    
+                return Promise.reject(new InvalidOperationException("using completed UnitOfWork"));
             return Promise.resolve(this._transactionScope.trx);
         }
-        
+
         let promise = new Promise<object>((resolve, reject) =>
         {
             this._dbConnectionFactory.create()
                 .then((knex: Knex) =>
                 {
-// tslint:disable-next-line: no-floating-promises
-                    knex.transaction((trx: Knex.Transaction) =>
-                    {
-                        if (this._transactionScope)
+
+                    knex
+                        .transaction((trx: Knex.Transaction) =>
                         {
-                            trx.rollback();
-                            if (this._transactionScope.isCommitted || this._transactionScope.isRolledBack)
-                                reject(new InvalidOperationException("using completed UnitOfWork")); 
+                            if (this._transactionScope)
+                            {
+                                trx.rollback();
+                                if (this._transactionScope.isCommitted || this._transactionScope.isRolledBack)
+                                    reject(new InvalidOperationException("using completed UnitOfWork"));
+                                else
+                                    resolve(this._transactionScope.trx);
+                            }
                             else
+                            {
+                                this._transactionScope = {
+                                    trx: trx,
+                                    isCommitted: false,
+                                    isRolledBack: false
+                                };
+
                                 resolve(this._transactionScope.trx);
-                        }
-                        else
-                        {
-                            this._transactionScope = {
-                                trx: trx,
-                                isCommitted: false,
-                                isRolledBack: false
-                            };
-                            
-                            resolve(this._transactionScope.trx);
-                        }
-                    });
+                            }
+                        })
+                        .then((...params: any[]) => console.log("knex transaction then", params))
+                        .catch((e) => console.warn("knex transaction catch", e));
                 })
                 .catch(err => reject(err));
         });
-        
+
         return promise;
     }
-    
+
     public commit(): Promise<void>
     {
         if (!this._transactionScope)
             return Promise.resolve();
-        
+
         if (this._transactionScope.isCommitted || this._transactionScope.isRolledBack)
-            return Promise.reject(new InvalidOperationException("committing completed UnitOfWork"));    
-        
+            return Promise.reject(new InvalidOperationException("committing completed UnitOfWork"));
+
         this._transactionScope.isCommitted = true;
         let promise = new Promise<void>((resolve, reject) =>
         {
@@ -79,10 +82,10 @@ export class KnexPgUnitOfWork implements UnitOfWork
                 .then(() => resolve())
                 .catch((err) => reject(err));
         });
-        
+
         return promise;
     }
-    
+
     public rollback(): Promise<void>
     {
         if (!this._transactionScope)
