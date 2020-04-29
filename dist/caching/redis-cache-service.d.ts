a387c5c453c1cf2aa1913926679d718ced185e9d
nivinjoseph <nivinjoseph@outlook.com>
2020-04-28 23:43:49 -0400

    preparing to publish new version

--- dist/caching/redis-cache-service.d.ts
+++ dist/caching/redis-cache-service.d.ts
@@ -1,10 +1,11 @@
 import { Disposable } from "@nivinjoseph/n-util";
+import * as Redis from "redis";
 import { CacheService } from "./cache-service";
 export declare class RedisCacheService implements CacheService, Disposable {
     private readonly _client;
     private _isDisposed;
     private _disposePromise;
-    constructor(redisUrl?: string);
+    constructor(redisClient: Redis.RedisClient);
     store<T>(key: string, value: T, expiry?: number): Promise<void>;
     retrieve<T>(key: string): Promise<T>;
     exists(key: string): Promise<boolean>;
--- dist/caching/redis-cache-service.js
+++ dist/caching/redis-cache-service.js
@@ -1,14 +1,24 @@
 "use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const Redis = require("redis");
 const n_defensive_1 = require("@nivinjoseph/n-defensive");
 const n_exception_1 = require("@nivinjoseph/n-exception");
-class RedisCacheService {
-    constructor(redisUrl) {
-        n_defensive_1.given(redisUrl, "redisUrl").ensureIsString();
-        this._client = Redis.createClient(redisUrl);
+const n_ject_1 = require("@nivinjoseph/n-ject");
+let RedisCacheService = class RedisCacheService {
+    constructor(redisClient) {
         this._isDisposed = false;
         this._disposePromise = null;
+        n_defensive_1.given(redisClient, "redisClient").ensureHasValue().ensureIsObject();
+        this._client = redisClient;
     }
     store(key, value, expiry) {
         return new Promise((resolve, reject) => {
@@ -90,10 +100,14 @@ class RedisCacheService {
     dispose() {
         if (!this._isDisposed) {
             this._isDisposed = true;
-            this._disposePromise = new Promise((resolve, _) => this._client.quit(() => resolve()));
+            this._disposePromise = Promise.resolve();
         }
         return this._disposePromise;
     }
-}
+};
+RedisCacheService = __decorate([
+    n_ject_1.inject("RedisClient"),
+    __metadata("design:paramtypes", [Redis.RedisClient])
+], RedisCacheService);
 exports.RedisCacheService = RedisCacheService;
 //# sourceMappingURL=redis-cache-service.js.map
