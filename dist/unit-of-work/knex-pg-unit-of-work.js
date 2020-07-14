21d3da8fc4a15df1cbdd276098b2c3b34587102a
nivinjoseph <nivinjoseph@outlook.com>
2020-07-14 06:05:27 -0400

    preparing to publish new version

--- dist/unit-of-work/knex-pg-unit-of-work.js
+++ dist/unit-of-work/knex-pg-unit-of-work.js
@@ -13,78 +13,75 @@ exports.KnexPgUnitOfWork = void 0;
 const n_defensive_1 = require("@nivinjoseph/n-defensive");
 const n_exception_1 = require("@nivinjoseph/n-exception");
 const n_ject_1 = require("@nivinjoseph/n-ject");
-let KnexPgUnitOfWork = (() => {
-    let KnexPgUnitOfWork = class KnexPgUnitOfWork {
-        constructor(dbConnectionFactory) {
-            n_defensive_1.given(dbConnectionFactory, "dbConnectionFactory").ensureHasValue();
-            this._dbConnectionFactory = dbConnectionFactory;
+let KnexPgUnitOfWork = class KnexPgUnitOfWork {
+    constructor(dbConnectionFactory) {
+        n_defensive_1.given(dbConnectionFactory, "dbConnectionFactory").ensureHasValue();
+        this._dbConnectionFactory = dbConnectionFactory;
+    }
+    getTransactionScope() {
+        if (this._transactionScope) {
+            if (this._transactionScope.isCommitted || this._transactionScope.isRolledBack)
+                return Promise.reject(new n_exception_1.InvalidOperationException("using completed UnitOfWork"));
+            return Promise.resolve(this._transactionScope.trx);
         }
-        getTransactionScope() {
-            if (this._transactionScope) {
-                if (this._transactionScope.isCommitted || this._transactionScope.isRolledBack)
-                    return Promise.reject(new n_exception_1.InvalidOperationException("using completed UnitOfWork"));
-                return Promise.resolve(this._transactionScope.trx);
-            }
-            let promise = new Promise((resolve, reject) => {
-                this._dbConnectionFactory.create()
-                    .then((knex) => {
-                    knex
-                        .transaction((trx) => {
-                        if (this._transactionScope) {
-                            trx.rollback();
-                            if (this._transactionScope.isCommitted || this._transactionScope.isRolledBack)
-                                reject(new n_exception_1.InvalidOperationException("using completed UnitOfWork"));
-                            else
-                                resolve(this._transactionScope.trx);
-                        }
-                        else {
-                            this._transactionScope = {
-                                trx: trx,
-                                isCommitted: false,
-                                isRolledBack: false
-                            };
+        let promise = new Promise((resolve, reject) => {
+            this._dbConnectionFactory.create()
+                .then((knex) => {
+                knex
+                    .transaction((trx) => {
+                    if (this._transactionScope) {
+                        trx.rollback();
+                        if (this._transactionScope.isCommitted || this._transactionScope.isRolledBack)
+                            reject(new n_exception_1.InvalidOperationException("using completed UnitOfWork"));
+                        else
                             resolve(this._transactionScope.trx);
-                        }
-                    })
-                        .then(() => { })
-                        .catch(() => { });
+                    }
+                    else {
+                        this._transactionScope = {
+                            trx: trx,
+                            isCommitted: false,
+                            isRolledBack: false
+                        };
+                        resolve(this._transactionScope.trx);
+                    }
                 })
-                    .catch(err => reject(err));
-            });
-            return promise;
-        }
-        commit() {
-            if (!this._transactionScope)
-                return Promise.resolve();
-            if (this._transactionScope.isCommitted || this._transactionScope.isRolledBack)
-                return Promise.reject(new n_exception_1.InvalidOperationException("committing completed UnitOfWork"));
-            this._transactionScope.isCommitted = true;
-            let promise = new Promise((resolve, reject) => {
-                this._transactionScope.trx.commit()
-                    .then(() => resolve())
-                    .catch((err) => reject(err));
-            });
-            return promise;
-        }
-        rollback() {
-            if (!this._transactionScope)
-                return Promise.resolve();
-            if (this._transactionScope.isCommitted || this._transactionScope.isRolledBack)
-                return Promise.reject(new n_exception_1.InvalidOperationException("rolling back completed UnitOfWork"));
-            this._transactionScope.isRolledBack = true;
-            let promise = new Promise((resolve, reject) => {
-                this._transactionScope.trx.rollback("[DELIBERATE]")
-                    .then(() => resolve())
-                    .catch((err) => reject(err));
-            });
-            return promise;
-        }
-    };
-    KnexPgUnitOfWork = __decorate([
-        n_ject_1.inject("DbConnectionFactory"),
-        __metadata("design:paramtypes", [Object])
-    ], KnexPgUnitOfWork);
-    return KnexPgUnitOfWork;
-})();
+                    .then(() => { })
+                    .catch(() => { });
+            })
+                .catch(err => reject(err));
+        });
+        return promise;
+    }
+    commit() {
+        if (!this._transactionScope)
+            return Promise.resolve();
+        if (this._transactionScope.isCommitted || this._transactionScope.isRolledBack)
+            return Promise.reject(new n_exception_1.InvalidOperationException("committing completed UnitOfWork"));
+        this._transactionScope.isCommitted = true;
+        let promise = new Promise((resolve, reject) => {
+            this._transactionScope.trx.commit()
+                .then(() => resolve())
+                .catch((err) => reject(err));
+        });
+        return promise;
+    }
+    rollback() {
+        if (!this._transactionScope)
+            return Promise.resolve();
+        if (this._transactionScope.isCommitted || this._transactionScope.isRolledBack)
+            return Promise.reject(new n_exception_1.InvalidOperationException("rolling back completed UnitOfWork"));
+        this._transactionScope.isRolledBack = true;
+        let promise = new Promise((resolve, reject) => {
+            this._transactionScope.trx.rollback("[DELIBERATE]")
+                .then(() => resolve())
+                .catch((err) => reject(err));
+        });
+        return promise;
+    }
+};
+KnexPgUnitOfWork = __decorate([
+    n_ject_1.inject("DbConnectionFactory"),
+    __metadata("design:paramtypes", [Object])
+], KnexPgUnitOfWork);
 exports.KnexPgUnitOfWork = KnexPgUnitOfWork;
 //# sourceMappingURL=knex-pg-unit-of-work.js.map
